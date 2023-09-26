34f99b5ad536653f12edc205318197be0772543e
Ethan Donahue <donahue.ethanj@gmail.com>
2023-09-26 19:26:44 -0400

    Revert "Added basic auth for login"

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const authRouter = require("./auth"); 
 const app = express();
 
 app.use(cors());
@@ -9,8 +8,6 @@ app.use(express.json());
 app.get("/message", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
-
-app.use("/auth", authRouter);
 
 app.listen(8000, () => {
   console.log(`Server is running on port 8000.`);
