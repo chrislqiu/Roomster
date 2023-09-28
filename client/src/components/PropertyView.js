8320315cfd43a954d6bd4c10b7aeff34f33fc078
Tea Lazareto <tlazaret@purdue.edu>
2023-09-28 19:54:16 -0400

    Worked on PropertyView and removed proxy:blahblah

--- client/src/components/PropertyView.js
+++ client/src/components/PropertyView.js
@@ -2,14 +2,16 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import { Box } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import Typography from '@mui/material/Typography';
+import { AspectRatio } from '@mui/joy';
+import Divider from '@mui/material/Divider';
+import {Stack} from '@mui/material';
 
-const PropertyViewMore = ({ text }) => {
+const PropertyViewMore = ({ text, data }) => {
     const [open, setOpen] = React.useState(false)
     const handleOpen = () => {
         setOpen(true)
@@ -17,43 +19,127 @@ const PropertyViewMore = ({ text }) => {
     const handleClose = () => {
         setOpen(false)
     }
-
     return (
         <React.Fragment>
-            <Button 
+            <Button
                 variant='contained'
                 onClick={handleOpen}
                 sx={{
                     ":hover": {
-                        bgcolor:"#F6EBE1",
-                        color:"#AB191F"
+                        bgcolor: "#F6EBE1",
+                        color: "#AB191F"
                     },
-                    backgroundColor:"#AB191F",
-                    color:"#F6EBE1"
+                    backgroundColor: "#AB191F",
+                    color: "#F6EBE1"
                 }}
-                >
+            >
                 {text}
             </Button>
             <Dialog
-            open={open}
-            onClose={handleClose}
-            sx={{
-                "& .MuiDialog-container": {
-                  "& .MuiPaper-root": {
-                    width: "100%",
-                    maxWidth: "500px",  
-                    maxHeight: "500px"
-                  },
-                },
-            }}
+                open={open}
+                onClose={handleClose}
+                sx={{
+                    "& .MuiDialog-container": {
+                        "& .MuiPaper-root": {
+                            width: "70%",
+                            maxWidth: 750,
+                            maxHeight: 500,
+                        },
+                    },
+
+                }}
             >
-                <DialogTitle sx={{fontWeight: 600}}> Property Details </DialogTitle>
-                <DialogContent sx={{width: '100%'}}>
+                <DialogContent>
+                    <AspectRatio minHeight={100} maxHeight={200} minWidth={300} maxWidth={400}>
+                            <img
+                                src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+                                srcSet="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2 2x"
+                                alt=""
+                            />
+                        </AspectRatio>
+                    <Stack direction='row' spacing={5} sx={{marginTop: 2}}>
                     <Box width='600'>
-                    <Typography>
-                        This is where stuff goes
-                    </Typography>
+                        <Typography
+                            sx={{
+                                fontWeight: 600, 
+                                marginTop: 1,
+                                fontSize: 20
+                            }}
+                        >
+                        {data.propertyName}
+                        </Typography>
+                        <Typography
+                            sx={{
+                                fontWeight: 300, 
+                                fontSize: 16,
+                                fontStyle: 'italic',
+                                marginTop: 1
+                            }}
+                        >
+                            {data.addr}
+                        </Typography>
+                        <Typography
+                            sx={{
+                                fontWeight: 300, 
+                                fontSize: 16,
+                            }}
+                        >
+                            {data.numBed} beds, {data.numBath} baths
+                        </Typography>
+                        <Divider orientation='horizontal' width={200} sx={{borderBottomWidth: 3, color:"#AB191F", backgroundColor:"#AB191F", marginY:1}}/>
+                        <Typography
+                            sx={{
+                                fontWeight: 500, 
+                                fontSize: 16,
+                                
+                            }}
+                        >
+                            ${data.cost} per month
+                        </Typography>
                     </Box>
+                    <Divider orientation='verticle' width={3} sx={{borderBottomWidth: 3, color:"#AB191F", backgroundColor:"#AB191F", marginY:2}}/>
+                    <Box width={'100%'}>
+                    <Typography
+                            sx={{
+                                fontWeight: 600, 
+                                marginTop: 1,
+                                fontSize: 16
+                            }}
+                        >
+                        Amenities
+                        </Typography>
+                        <Typography
+                            sx={{
+                                fontWeight: 300, 
+                                fontSize: 16,
+                            }}
+                        >
+                            Parking {<br/>}
+                            Pets {<br/>}
+                            Furnished apartment {<br/>}
+                        </Typography>
+                    </Box>
+                    <Divider orientation='verticle' width={3} sx={{borderBottomWidth: 3, color:"#AB191F", backgroundColor:"#AB191F", marginY:2}}/>
+                    <Box width={'100%'}>
+                    <Typography
+                            sx={{
+                                fontWeight: 600, 
+                                marginTop: 1,
+                                fontSize: 16
+                            }}
+                        >
+                        Contact Information
+                        </Typography>
+                        <Typography
+                            sx={{
+                                fontWeight: 300, 
+                                fontSize: 16,
+                            }}
+                        >
+                            Company Name {<br/>}
+                        </Typography>
+                    </Box>
+                    </Stack>
                 </DialogContent>
                 <DialogActions>
                     <IconButton
--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,12 +1,31 @@
 import PropertyViewMore from "../components/PropertyView";
+import React from "react"
+
 import { Container, Box } from "@mui/material";
+
 const MainPage = () => {
+    const [propertyInfo, setPropertyInfo] = React.useState([])
+    React.useEffect(() => {
+        const getPropertyInfo = async () => {
+            const res = await fetch('http://localhost:8000/cards/all-cards')
+            const getData = await res.json()
+            const obj = JSON.parse(JSON.stringify(getData));
+            setPropertyInfo(obj);
+        }
+        getPropertyInfo()
+    }, [])
     return (
-    <Container sx={{width: '100%'}}>
-        <Box sx={{m: 1}}>
-        <PropertyViewMore text={"This is where the property card box will go"}/>
-        </Box>
-    </Container>
+        <Container sx={{ width: '100%' }}>
+            {console.log(propertyInfo)}
+            <Box sx={{ m: 1 }}>
+                {
+                    propertyInfo.map(cards => {
+                        return <PropertyViewMore text={"This is where the property card box will go"} data={cards} />
+                        }
+                    )
+                }
+            </Box>
+        </Container>
     )
 }
 export default MainPage;
