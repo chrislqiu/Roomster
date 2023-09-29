cacafc1b8953d355e42e1c3464fe4696477278fc
Rachel La <rachel.la1807@gmail.com>
2023-09-29 19:05:04 -0400

    created card and added property detail

--- client/src/components/PropertyView.js
+++ client/src/components/PropertyView.js
@@ -19,11 +19,11 @@ const PropertyViewMore = ({ text }) => {
     const handleClose = () => {
         setOpen(false)
     }
-
+    
     const styles = {
-        media: {
-            borderRadius: "10px",
-            padding: "10px"
+        divider: {
+            borderTop: "3px solid #AB191F",
+            padding: "0"
         }
     }
 
@@ -44,20 +44,21 @@ const PropertyViewMore = ({ text }) => {
                     height: "300px",
                     boxShadow: "0px 0px 3px 3px rgba(0, 0, 0, .1)"
                 }}>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image={imgExample}
-                        style={styles.media}
+                <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            image={imgExample}
+                            height="140"
+                        />
+                        <CardContent>
+                          <Typography variant="h6" style={{margin: "-10px 0 0px 0"}}>Company Name</Typography>
+                          <Typography variant="body2" style={{margin: "0 0 10px 0"}}> 123 Address</Typography>
+                          <Typography variant="body2"> # bedroom</Typography>
+                          <Typography variant="body2"> # bathroom</Typography>
+                          <hr style={styles.divider}></hr>
                         
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h6" component="div">
-                            CompanyName
-                        </Typography>
                     </CardContent>
-                
-                {text}
+                </CardActionArea>
             </Card>
             <Dialog
             open={open}
--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -4,7 +4,7 @@ const MainPage = () => {
     return (
     <Container sx={{width: '100%'}}>
         <Box sx={{m: 1}}>
-        <PropertyViewMore text={"This is where the property card box will go"}/>
+        <PropertyViewMore text={"CompanyName"}/>
         </Box>
     </Container>
     )
