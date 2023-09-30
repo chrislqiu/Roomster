4f6b843d3ed7533caf14e121a3c4fb2d7889ff5f
Rachel La <rachel.la1807@gmail.com>
2023-09-30 10:51:28 -0400

    put cards into grid view on home page

--- client/src/components/PropertyView.js
+++ client/src/components/PropertyView.js
@@ -2,8 +2,6 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import { Box, Card, CardContent, CardMedia } from '@mui/material';
 import { CardActionArea } from '@mui/material';
-import { Box, Card, CardContent, CardMedia } from '@mui/material';
-import { CardActionArea } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
