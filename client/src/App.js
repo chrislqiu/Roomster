37db911a60a1f99a07cbcfa33f6bbf421541ee8b
Rachel La <rachel.la1807@gmail.com>
2023-10-07 14:24:32 -0400

    created My Coops page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import MainPage from "./pages/MainPage";
 import RenterPage from "./pages/RenterPage";
 import PropertyManagerPage from "./pages/ProperyManagerPage";
 import RenterCreateAccountPage from "./pages/RenterCreateAccountPage";
+import MyCoopsPage from "./pages/MyCoopsPage";
 import LoginPage from "./pages/LoginPage"
 import GlobalStyles from "@mui/material/GlobalStyles";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
@@ -117,9 +118,11 @@ class App extends React.Component {
             <Route path="/Home" element={<MainPage />} />
             <Route path="/RProfile" element={<RenterPage />} />
             <Route path="/MProfile" element={<PropertyManagerPage />} />
+            <Route path="/MyCoops" element={<MyCoopsPage />} />
             <Route path="/Login" element={<LoginPage />} />
             <Route path="/Settings" element={<Settings />} />
             <Route path="/RCreate" element={<RenterCreateAccountPage />} />
+            {/* {<Route path="/Login" element={<LoginPage />} />} */}
           </Routes>
         </BrowserRouter>
       </body>  
