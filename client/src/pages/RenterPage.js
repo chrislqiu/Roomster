42edf842c2468e408932b959e4663f610dcf2df9
Rachel La <rachel.la1807@gmail.com>
2023-10-07 14:03:24 -0400

    completed user info display

--- client/src/pages/RenterPage.js
+++ client/src/pages/RenterPage.js
@@ -1,6 +1,6 @@
-import { Grid, Card, Container, Box, Typography, CardContent, Input, Divider, TextField, Link, Button } from "@mui/material";
+import { InputBase, Grid, Card, Container, Box, Typography, CardContent, Input, Divider, TextField, Link, Button } from "@mui/material";
 import React from "react"
-import profilePic from "../images/profile-pic.png"
+import profilePic from "../images/profile-pic-no-shadow.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMars } from '@fortawesome/free-solid-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
@@ -28,6 +28,7 @@ const RenterPage = () => {
             //backgroundColor: "lightgreen"
         },
         header: {
+            margin: "10px 0 5px 0",
             fontWeight: "600",
             fontSize: "15pt",
             color: "#AB191F",
@@ -60,7 +61,20 @@ const RenterPage = () => {
             fontWeight: "600", 
             fontSize: "14pt", 
             color: "#AB191F",
-            padding: "0 0 0 25px"
+            padding: "0 0 0 25px",
+            margin: "0 0 10px 0",
+        },
+    }
+    const inputBaseSX = {
+        margin: "0 0 10px 25px", 
+        width:"300px", 
+        height: "35px",
+        borderRadius: "5px",
+        border: "2px solid #AB191F",
+        padding: "5px",
+        "&:hover": {
+            border: "2px solid #AB191F",
+            boxShadow:"0px 0px 3px 3px rgba(0, 0, 0, .1)", 
         }
     }
     return (
@@ -81,25 +95,19 @@ const RenterPage = () => {
                         <Typography style={Object.assign(styles.age, {padding: "0 10px 0 20px", marginTop: "20px"})}> 
                             {"22"}
                         </Typography >
-                        <FontAwesomeIcon icon={faMars} style={Object.assign(styles.icon, {marginTop: "20px"})}/>
+                        <FontAwesomeIcon icon={faMars} style={Object.assign(styles.icon, {marginTop: "22px"})}/>
                         
                     </Container>
                     <Typography style={styles.subheader}> 
                         {"Contact info"}
                     </Typography >
-                    <TextField variant="outlined"
-                    sx = {{boxShadow:"3", margin:"dense", marginBottom:"15px"}}
-                    />
-                    <Container style={styles.box}>
-                        
-
-                    </Container>
-                    
-
+                    <InputBase placeholder="Purdue Email" id="email-textfield" sx={inputBaseSX}/>
+                    <InputBase placeholder="Phone number" id="number-textfield" sx={inputBaseSX}/>
+                    <Button variant="contained" style={{backgroundColor: "#AB191F", float: "right", margin: "0 35px 0 0"}}>SAVE</Button>
                 </Box>
                 <Box width='100%' style={styles.column2}>
                     <Typography style={styles.header}> 
-                        {"USER INFORMATION"}
+                        {"FINDING COOPMATES?"}
                     </Typography >
                 </Box>
                 
