2ba4e828173c8a7ed0baa29a3e5a09280bb5d729
Ethan Donahue <donahue.ethanj@gmail.com>
2023-11-14 18:42:47 -0500

    Add user cards

--- client/src/components/AdminUserView.js
+++ client/src/components/AdminUserView.js
@@ -2,38 +2,97 @@ import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import { List, ListItem, Box, CardMedia, IconButton, Tooltip, } from '@mui/material';
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import CheckIcon from "@mui/icons-material/Check";
+import LockResetIcon from '@mui/icons-material/LockReset';
+
 
 const AdminUserView = ({ username, userType }) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+
   return (
-    <Card
-      variant='contained'
-      alignContent='center'
-      sx={{
-        ":hover": {
-          bgcolor: "#AB191F",
-          color: "#f5ebe0",
-          cursor: "pointer",
-        },
-        backgroundColor: "#f5ebe0",
-        color: "#AB191F",
-        width: "250px",
-        height: "100px",
-        marginBottom: "20px",
-        marginRight: "20px",
-        marginLeft: "20px",
-        borderRadius: "10px",
-        boxShadow: "0px 0px 3px 3px rgba(0, 0, 0, .1)"
-      }}
-    >
-      <CardContent>
-        <Typography variant="h5" component="div">
-          {username}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {userType}
-        </Typography>
-      </CardContent>
-    </Card>
+    <React.Fragment>
+      <Card
+        variant='contained'
+        alignContent='center'
+        onClick={() => {
+          handleOpen();
+        }}
+        sx={{
+          ":hover": {
+            bgcolor: "#AB191F",
+            color: "#f5ebe0",
+            cursor: "pointer",
+          },
+          backgroundColor: "#f5ebe0",
+          color: "#AB191F",
+          width: "300px",
+          height: "100px",
+          marginBottom: "20px",
+          marginRight: "20px",
+          marginLeft: "20px",
+          borderRadius: "10px",
+          boxShadow: "0px 0px 3px 3px rgba(0, 0, 0, .1)"
+        }}
+      >
+        <CardContent>
+          <Typography variant="h6" component="div" sx={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>
+            {username}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {userType}
+          </Typography>
+        </CardContent>
+      </Card>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        sx={{
+          "& .MuiDialog-container": {
+            "& .MuiPaper-root": {
+              width: "70%",
+              maxWidth: 500,
+              maxHeight: 300,
+              backgroundColor: "#F6EBE1"
+            },
+          },
+        }}
+      >
+        <DialogContent>
+          <Typography variant="h6" component="div" sx={{ color: "#AB191F", overflow: 'hidden', textOverflow: 'ellipsis' }}>
+            {username}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {userType}
+          </Typography>
+          <Box sx={{ display: 'flex', justifyContent: 'space-around', marginTop: '10px' }}>
+          <Tooltip title="Delete User Account">
+            <IconButton onClick={null}>
+              <DeleteOutlineIcon sx={{ color: "#AB191F" }} />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Reset User Password">
+            <IconButton onClick={null}>
+              <LockResetIcon sx={{ color: "#AB191F" }} />
+            </IconButton>
+          </Tooltip>
+          </Box>
+        </DialogContent>
+      </Dialog>
+
+    </React.Fragment>
+
   );
 }
 
--- client/src/pages/AdminUserPage.js
+++ client/src/pages/AdminUserPage.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Dialog, DialogContent, DialogActions, Tooltip, IconButton, Avatar, InputBase, Slider, Select, MenuItem, Grid, Card, Container, Box, Typography, CardContent, Radio, Button, RadioGroup, FormControl, FormControlLabel, CardMedia } from "@mui/material";
 import AdminUserView from '../components/AdminUserView'
+import AdminUserPlaceholder from '../components/AdminUserPlaceholder';
 
 
 const AdminUserPage = () => {
+    const [renters, setRenters] = useState([]);
+    const [managers, setManagers] = useState([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const rentersData = await renterUsers();
+            const managersData = await managerUsers();
+
+            setRenters(rentersData);
+            setManagers(managersData);
+        };
+
+        fetchUsers();
+    }, []);
+
+    const renterUsers = async () => {
+        try {
+            const response = await fetch('http://localhost:8000/auth/renters', {
+                method: 'GET',
+                credentials: 'include',
+            });
+
+            if (response.ok) {
+                const renters = await response.json();
+                console.log('Gathered renters');
+                return renters;
+            } else {
+                console.log('Failed to gather renters');
+                return [];
+            }
+        } catch (error) {
+            console.error('Error gathering users:', error);
+            return [];
+        }
+    };
+    const managerUsers = async () => {
+        try {
+            const response = await fetch('http://localhost:8000/auth/managers', {
+                method: 'GET',
+                credentials: 'include',
+            });
+
+            if (response.ok) {
+                const managers = await response.json();
+                console.log('Gathered managers');
+                return managers;
+            } else {
+                console.log('Failed to gather managers');
+                return [];
+            }
+        } catch (error) {
+            console.error('Error gathering users:', error);
+            return [];
+        }
+    };
+
+
     const styles = {
         feed: {
             display: "flex",
@@ -15,8 +73,25 @@ const AdminUserPage = () => {
     }
     return (
         <Container sx={{ width: '100%' }}>
+            <Typography sx={{ fontSize: "30px", textAlign: "center" }}>
+                Users
+            </Typography>
             <Box sx={{ m: 1 }} style={styles.feed}>
-                <AdminUserView username="john@purdue.edu" userType="Renter" />
+                {renters.length > 0 ? (
+                    renters.map((renter) => (
+                        <AdminUserView key={renter._id} username={renter.username} userType="Renter" />
+                    ))
+                ) : (
+                    <AdminUserPlaceholder />
+                )}
+
+                {managers.length > 0 ? (
+                    managers.map((manager) => (
+                        <AdminUserView key={manager._id} username={manager.username} userType="Manager" />
+                    ))
+                ) : (
+                    <AdminUserPlaceholder />
+                )}
             </Box>
         </Container>
     );
