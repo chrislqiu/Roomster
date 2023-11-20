fb9744a902c01facc4cc98d408f4a43811b63665
Jillian Urgello <jillian.urgello@gmail.com>
2023-11-20 14:50:18 -0600

    - mostly done with UI for manager tour requests

--- client/src/pages/TourRequestsPage.js
+++ client/src/pages/TourRequestsPage.js
@@ -4,24 +4,41 @@ import { createTheme, ThemeProvider, useTheme } from '@mui/material/styles';
 
 
 const TourRequestsPage = ({ login }) => {
+    const theme = useTheme();
+
 
     const styles = {
-        subheader: {
+        header: {
             fontWeight: "600",
             fontSize: "15pt",
             color: "secondaryColor",
             padding: "0 740px 10px 0",
         },
 
+        subheader: {
+            fontWeight: "600",
+            fontSize: "13pt",
+            color: "textColor",
+            backgroundColor:"primaryColor",
+            padding: "5px 15px 5px 15px",
+            borderBottomColor:"secondaryColor",
+            borderBottomWidth:"3px"
+        },
+
+        cells: {
+            fontWeight:"400",
+            fontSize:"11pt",
+            color: "textColor",
+        }
     }
 
     const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [rowsPerPage, setRowsPerPage] = React.useState(100);
 
 
     const columns = [
-        { id: 'date', label: 'DATE', width: "100px"},
-        { id: 'time', label: 'TIME', width: "100px" },
+        { id: 'date', label: 'DATE', width: "100px", align:'center'},
+        { id: 'time', label: 'TIME', width: "100px", align:'center'},
         {
           id: 'property',
           label: 'PROPERTY',
@@ -42,61 +59,50 @@ const TourRequestsPage = ({ login }) => {
         },
       ];
       
-    function createData(name, code, population, size) {
-    const density = population / size;
-    return { name, code, population, size, density };
+    function pullData(date, time, property, user, status) {
+        return { date, time, property, user, status };
     }
     
-    /* Pull data from DB, dummy data for now */
+    /* Pull data from DB, dummy data for now. Note that properties should be specific to company */
     const rows = [
-       
-    createData('India', 'IN', 1324171354, 3287263),
-    createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
+        pullData('10/21/23', '3:00PM', 'Studio A', 'Chicken A', 'APPROVED'),
+        pullData('01/02/24', '11:00AM', 'Penthouse', 'Duck B', 'APPROVED'),
+        pullData('10/22/23', '12:00PM', 'Studio B', 'Chicken B', 'PENDING'),
+        pullData('10/23/23', '1:00PM', 'Studio C', 'Chicken C', 'DECLINED'),
+        pullData('10/23/23', '10:00AM', 'Studio A', 'Cow A', 'PENDING'),
+        pullData('10/25/23', '11:00AM', 'Penthouse', 'Cow B', 'APPROVED'),
+        pullData('10/21/23', '3:00PM', 'Studio A', 'Pig A', 'APPROVED'),
+        pullData('10/22/23', '12:00PM', 'Studio B', 'Pig B', 'PENDING'),
+        pullData('10/23/23', '1:00PM', 'Studio C', 'Sheep C', 'DECLINED'),
+        pullData('10/23/23', '10:00AM', 'Studio A', 'Duck A', 'PENDING'),
+        pullData('10/25/23', '11:00AM', 'Penthouse', 'Duck B', 'APPROVED')
     ];
 
 
     return (
         <Grid container spacing={0} direction="column" alignItems="center" justify="center">
 
-            <Typography sx={styles.subheader}>
+            <Typography sx={styles.header}>
                 TOUR REQUESTS
             </Typography>
             <Paper sx={{
                 width: '900px', 
                 height:"300px", 
                 overflow: 'hidden', 
-                backgroundColor:"primaryColor"
-                }}>
-                <TableContainer sx={{ maxHeight: 440, borderColor:"blue"}}>
+                backgroundColor:"primaryColor",
+                boxShadow: theme.palette.type === 'light' ? "0px 0px 3px 3px rgba(0, 0, 0, .1)" : "0px 0px 3px 3px rgba(245, 235, 224, .1)",
+            }}>
+                <TableContainer sx={{ maxHeight: "440px"}}>
                     <Table stickyHeader aria-label="sticky table">
                         <TableHead >
-                            <TableRow sx={{backgroundColor:"blue"}}>
+                            <TableRow >
                             {columns.map((column) => (
                                 <TableCell
                                     key={column.id}
                                     align={column.align}
+                                    sx={styles.subheader}
                                     style={{
-                                        backgroundColor:"#F6EBE1",
-                                        fontWeight: "600",
-                                        fontSize: "13pt",
-                                        color: "black",
-                                        //padding: "5px 15px 5px 15px",
-                                        minWidth:column.width,
-                                        borderBottomColor:"#AB191F",
-                                        borderBottomWidth:"3px"
+                                        minWidth:column.width
                                     }}
                                 >
                                 {column.label}
@@ -112,12 +118,10 @@ const TourRequestsPage = ({ login }) => {
                                 return (
                                 <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
                                     {columns.map((column) => {
-                                    const value = row[column.id];
+                                    const tourRequest = row[column.id];
                                     return (
-                                        <TableCell key={column.id} align={column.align}>
-                                        {column.format && typeof value === 'number'
-                                            ? column.format(value)
-                                            : value}
+                                        <TableCell key={column.id} align={column.align} sx={styles.cells}>
+                                            {tourRequest}
                                         </TableCell>
                                     );
                                     })}
@@ -129,49 +133,6 @@ const TourRequestsPage = ({ login }) => {
             </TableContainer>
         </Paper>
     </Grid>
-
-        // <Grid container spacing={2} columns={7} direction="column" alignItems="center" justify="center">
-        //     <List
-        //         sx={{
-        //         width: '100%',
-        //         width:"1000px",
-        //         //maxWidth: 360,
-        //         bgcolor: 'background.paper',
-        //         position: 'relative',
-        //         overflow: 'auto',
-        //         maxHeight: 300,
-        //         '& ul': { padding: 0 },
-        //         }}
-        //         subheader={<li />}
-        //     >
-        //         <li>
-        //             <ListSubheader sx={{height:"60px", marginBottom:"0px", backgroundColor:"pink"}}>
-        //                 <Typography sx={styles.subheader}>
-        //                     DATE
-        //                 </Typography>
-        //                 <Typography sx={styles.subheader}>
-        //                     TIME
-        //                 </Typography>
-        //                 <Divider variant="middle" sx={{borderBottomWidth: 3, backgroundColor: "secondaryColor", marginY: 1 }} />
-        //             </ListSubheader>
-        //             <ul>
-        //                 <Grid container item xs={6} direction="column">
-        //                 <ListItem  sx={{paddingLeft:"15px",paddingRight:"15px"}}>
-        //                     <ListItemText>
-        //                         <Typography >THIS IS AN ITEM</Typography>
-        //                     </ListItemText>
-        //                     <ListItemText>
-        //                         <Typography>AN ITEMMMMMMMMMMMM</Typography>
-        //                     </ListItemText>
-        //                     <ListItemText>
-        //                         <Typography >THIS IS AN ITEM</Typography>
-        //                     </ListItemText>
-        //                 </ListItem>
-        //                 </Grid>
-        //             </ul>
-        //         </li>
-        //     </List>
-        // </Grid>
     )
     
 }
