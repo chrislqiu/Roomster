fe67f2ce4433e4938e01537167c57ff8df3878af
Elasia Rodriguez <elasiaruth@gmail.com>
2023-11-27 12:08:06 -0500

    share button and tour model

--- client/src/components/PropertyView.js
+++ client/src/components/PropertyView.js
@@ -17,6 +17,7 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import CheckIcon from "@mui/icons-material/Check";
+import SendIcon from '@mui/icons-material/Send';
 import { useNavigate, createSearchParams } from 'react-router-dom';
 import AddCoopView from './AddCoopView';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -202,6 +203,14 @@ const PropertyViewMore = ({ data, featured, favCoops, myCoops, login, admin }) =
     const pullUtilities = () => {
         setUtilities(Object.keys(utilities).filter(key => utilities[key] === true))
     }
+
+    const handleShare = async () => {
+        const id = data._id;
+
+        console.log("data: " + id);
+        const link = "http://localhost:3001/"
+        //window.location.href
+    };
 
     const handleDeleteProperty = async () => {
         const id = data._id;
@@ -601,6 +610,15 @@ const PropertyViewMore = ({ data, featured, favCoops, myCoops, login, admin }) =
                     </Stack>
                 </DialogContent>
                 <DialogActions>
+
+                    <IconButton
+                        style={{ position: "BottomLeft", position: "sticky", top: 70, left: 0 }}
+                        //onClick={() => handleShare()}
+                    >
+                        <SendIcon
+                            sx={{ color: "textColor" }}
+                        />
+                    </IconButton>
 
                     {console.log(isVerified)}
                     {isVerified === true ?
